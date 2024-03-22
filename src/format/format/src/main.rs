bb4131e44e44f64fa9c0b076bcc49c2cc23d437d
Miro Benicio <mirodantas27@gmail.com>
2024-03-22 13:58:21 -0300

    feat: process line items

--- src/format/format/src/main.rs
+++ src/format/format/src/main.rs
@@ -319,6 +319,7 @@ pub struct StateResponseBlock {
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct LineItemGroup {
+    #[serde(rename = "LineItemGroupIndex")]
     line_item_group_index: i64,
     line_items: Vec<LineItem>,
 }
@@ -333,7 +334,7 @@ pub struct LineItem {
 #[serde(rename_all = "PascalCase")]
 pub struct LineItemExpenseField {
     page_number: i64,
-    #[serde(rename = "line_item_type")]
+    #[serde(rename = "Type")]
     line_item_expense_field_type: TypeClass,
     value_detection: Detection,
 }
@@ -396,56 +397,51 @@ fn process_blocks(blocks: &Vec<Block>) -> Vec<StateResponseBlock> {
 
 // Function to process LineItems
 fn process_line_item_groups(line_item_groups: &Vec<LineItemGroup>) -> Vec<StateResponseLineItemGroup> {
+    println!("estou aqui!!!!");
     line_item_groups.iter().map(|line_item_group| {
         StateResponseLineItemGroup {
             line_item_group_index: line_item_group.line_item_group_index,
-            line_items: StateResponseLineItem {
-                line_item_expense_fields: line_item_group.line_items.iter().map(|line_item| {
-                
+            line_items: line_item_group.line_items.iter().map(|line_item| {
+                let line_item_expense_fields: Vec<StateResponseLineItemExpenseField> = line_item.line_item_expense_fields.iter().map(|field| {
                     let mut expense_field = StateResponseLineItemExpenseField {
                         product_code: None,
                         item: None,
                         expense_row: None,
                         price: None
-
                     };
-                    
-                    line_item.line_item_expense_fields.iter().map(|field| {
-                        
-                        match field.line_item_expense_field_type.text.as_str()  {
-                            "PRODUCT_CODE" =>  expense_field.product_code = Some(StateResponseSummaryField {
-                                confidence_key: field.line_item_expense_field_type.confidence,
-                                confidence_value: field.value_detection.confidence,
-                                value: field.value_detection.text.clone(),
-                            }),
-                            "ITEM" =>  expense_field.item = Some(StateResponseSummaryField {
-                                confidence_key: field.line_item_expense_field_type.confidence,
-                                confidence_value: field.value_detection.confidence,
-                                value: field.value_detection.text.clone(),
-                            }),
-                            "EXPENSE_ROW" =>  expense_field.expense_row = Some(StateResponseSummaryField {
-                                confidence_key: field.line_item_expense_field_type.confidence,
-                                confidence_value: field.value_detection.confidence,
-                                value: field.value_detection.text.clone(),
-                            }),
-                            "PRICE" =>  expense_field.price = Some(StateResponseSummaryField {
-                                confidence_key: field.line_item_expense_field_type.confidence,
-                                confidence_value: field.value_detection.confidence,
-                                value: field.value_detection.text.clone(),
-                            }),
-                            _ => {} // Handling other cases if needed
-                        }
-                        
-                        
-                        // field = expense_field
-                    }).collect();
-    
-                
-                }).collect()
-            }
+                    println!("estou aqui 2!!!!");
+
+                    match field.line_item_expense_field_type.text.as_str()  {
+                        "PRODUCT_CODE" =>  expense_field.product_code = Some(StateResponseSummaryField {
+                            confidence_key: field.line_item_expense_field_type.confidence,
+                            confidence_value: field.value_detection.confidence,
+                            value: field.value_detection.text.clone(),
+                        }),
+                        "ITEM" =>  expense_field.item = Some(StateResponseSummaryField {
+                            confidence_key: field.line_item_expense_field_type.confidence,
+                            confidence_value: field.value_detection.confidence,
+                            value: field.value_detection.text.clone(),
+                        }),
+                        "EXPENSE_ROW" =>  expense_field.expense_row = Some(StateResponseSummaryField {
+                            confidence_key: field.line_item_expense_field_type.confidence,
+                            confidence_value: field.value_detection.confidence,
+                            value: field.value_detection.text.clone(),
+                        }),
+                        "PRICE" =>  expense_field.price = Some(StateResponseSummaryField {
+                            confidence_key: field.line_item_expense_field_type.confidence,
+                            confidence_value: field.value_detection.confidence,
+                            value: field.value_detection.text.clone(),
+                        }),
+                        _ => {} // Handling other cases if needed
+                    }
+                    expense_field
+                }).collect();
+                StateResponseLineItem { line_item_expense_fields }
+            }).collect()
         }
     }).collect()
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -562,9 +558,7 @@ mod tests {
 
     #[test]
     fn test_process_line_items() {
-        let json_str = r#"
-        "LineItemGroups": [
-            {
+        let json_str = r#" {
                 "LineItemGroupIndex": 1,
                 "LineItems": [
                     {
@@ -1025,15 +1019,51 @@ mod tests {
                     }
                 ]
             }
-        ],
         "#;
-        
-        let lineItems: LineItemGroup = serde_json::from_str(json_str).unwrap();
 
-        let result = process_line_item_groups(&vec![lineItems]);
+        let line_items: LineItemGroup = serde_json::from_str(json_str).unwrap();
 
-      
+        let result = process_line_item_groups(&vec![line_items]);
+
+        assert_eq!(result.len(), 1);
+
+        assert_eq!(result[0].line_item_group_index, 1);
+
+        assert_eq!(result[0].line_items.len(), 3);
+
+        let line_item_expense_fields = &result[0].line_items[0].line_item_expense_fields;
+        assert_eq!(line_item_expense_fields.len(), 4); 
+
+        for field in line_item_expense_fields {
+            match field.product_code {
+                Some(ref summary_field) => {
+                    assert_eq!(summary_field.value, "020108870398");
+                }
+                None => {}
+            }
+            match field.item {
+                Some(ref summary_field) => {
+                    assert_eq!(summary_field.value, "6 WING PLATE");
+                }
+                None => {}
+            }
+            match field.price {
+                Some(ref summary_field) => {
+                    assert_eq!(summary_field.value, "3.98");
+                }
+                None => {}
+            }
+            match field.expense_row {
+                Some(ref summary_field) => {
+                    assert_eq!(
+                        summary_field.value,
+                        "6 WING PLATE 020108870398 3.98 P"
+                    );
+                }
+                None => {}
+            }
         }
+    }
 }  
 
     
