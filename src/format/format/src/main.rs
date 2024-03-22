41a9fbb63c10b670d70342f0a1071f70510be698
Miro Benicio <mirodantas27@gmail.com>
2024-03-22 12:24:02 -0300

    wip: format line items

--- src/format/format/src/main.rs
+++ src/format/format/src/main.rs
@@ -397,46 +397,52 @@ fn process_blocks(blocks: &Vec<Block>) -> Vec<StateResponseBlock> {
 // Function to process LineItems
 fn process_line_item_groups(line_item_groups: &Vec<LineItemGroup>) -> Vec<StateResponseLineItemGroup> {
     line_item_groups.iter().map(|line_item_group| {
-        // let mut line_items_vec = Vec::new();
-        let line_items = line_item_group.line_items.iter().map(|line_item| {
-            // let mut expense_field = StateResponseLineItemExpenseField {};
-
-            let line_item_expense_fields = line_item.line_item_expense_fields.iter().map(|field| {
-                match field.line_item_expense_field_type.text.as_str()  {
-                    "PRODUCT_CODE" => expense_field.product_code = Some(StateResponseSummaryField {
-                        confidence_key: field.line_item_expense_field_type.confidence,
-                        confidence_value: field.value_detection.confidence,
-                        value: field.value_detection.text.clone(),
-                    }),
-                    "ITEM" => expense_field.item = Some(StateResponseSummaryField {
-                        confidence_key: field.line_item_expense_field_type.confidence,
-                        confidence_value: field.value_detection.confidence,
-                        value: field.value_detection.text.clone(),
-                    }),
-                    "EXPENSE_ROW" => expense_field.expense_row = Some(StateResponseSummaryField {
-                        confidence_key: field.line_item_expense_field_type.confidence,
-                        confidence_value: field.value_detection.confidence,
-                        value: field.value_detection.text.clone(),
-                    }),
-                    "PRICE" => expense_field.price = Some(StateResponseSummaryField {
-                        confidence_key: field.line_item_expense_field_type.confidence,
-                        confidence_value: field.value_detection.confidence,
-                        value: field.value_detection.text.clone(),
-                    }),
-                    _ => {} // Handling other cases if needed
-                }
-            }).collect();
-
-            // line_items_vec.push(expense_field);
-
-            StateResponseLineItem {
-                line_item_expense_fields: line_item_expense_fields,
-            }
-        }).collect();
-
         StateResponseLineItemGroup {
             line_item_group_index: line_item_group.line_item_group_index,
-            line_items: line_items,
+            line_items: StateResponseLineItem {
+                line_item_expense_fields: line_item_group.line_items.iter().map(|line_item| {
+                
+                    let mut expense_field = StateResponseLineItemExpenseField {
+                        product_code: None,
+                        item: None,
+                        expense_row: None,
+                        price: None
+
+                    };
+                    
+                    line_item.line_item_expense_fields.iter().map(|field| {
+                        
+                        match field.line_item_expense_field_type.text.as_str()  {
+                            "PRODUCT_CODE" =>  expense_field.product_code = Some(StateResponseSummaryField {
+                                confidence_key: field.line_item_expense_field_type.confidence,
+                                confidence_value: field.value_detection.confidence,
+                                value: field.value_detection.text.clone(),
+                            }),
+                            "ITEM" =>  expense_field.item = Some(StateResponseSummaryField {
+                                confidence_key: field.line_item_expense_field_type.confidence,
+                                confidence_value: field.value_detection.confidence,
+                                value: field.value_detection.text.clone(),
+                            }),
+                            "EXPENSE_ROW" =>  expense_field.expense_row = Some(StateResponseSummaryField {
+                                confidence_key: field.line_item_expense_field_type.confidence,
+                                confidence_value: field.value_detection.confidence,
+                                value: field.value_detection.text.clone(),
+                            }),
+                            "PRICE" =>  expense_field.price = Some(StateResponseSummaryField {
+                                confidence_key: field.line_item_expense_field_type.confidence,
+                                confidence_value: field.value_detection.confidence,
+                                value: field.value_detection.text.clone(),
+                            }),
+                            _ => {} // Handling other cases if needed
+                        }
+                        
+                        
+                        // field = expense_field
+                    }).collect();
+    
+                
+                }).collect()
+            }
         }
     }).collect()
 }
